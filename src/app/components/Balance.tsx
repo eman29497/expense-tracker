a9efc2d3357ce403736c619321c3845528fd64d6
eman <emansajjad863@gmail.com>
2026-03-31 05:17:25 -0700

    Fixed React import

--- src/app/components/Balance.tsx
+++ src/app/components/Balance.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'React';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 export default function Balance(){
